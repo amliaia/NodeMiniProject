17397ad17f18e6be591b23185eee5271838bcd7f
Arti <arti.iosdev@gmail.com>
2021-02-27 11:27:03 -0800

    show info

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { profile } = require('console');
+
+const user = {
+    name: "",
+    location: "",
+    github: "",
+    bio: "",
+    linkedin: ""
+  };
 
 inquirer
     .prompt([
@@ -29,8 +38,73 @@ inquirer
             name: 'github',
         },
     ])
-    .then((response) =>
-        response.confirm === response.password
-            ? console.log('Success!')
-            : console.log('You forgot your password already?!')
-    );
+
+    /*profile.then((data) => {
+  //  profile.then(function(data){
+        const html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+            <link rel="stylesheet" type="text/css" href="./style.css">
+        </head>
+        <body>
+        <h1 class="profile">${data.name}</h1>
+        <div class="profile">${data.location}</div>
+        <div class="profile">${data.bio}</div>
+        <div class="profile">${data.linkedin}</div>
+        <div class="profile">${data.github}</div>
+
+        <script type="text/javascript" src="script.js"></script>
+        </body>
+        </html>  
+    `;
+        fs.writeFile('index.html', html, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+    
+            console.log('Success!');
+        });
+    })
+*/
+
+
+    .then((response) => {
+        const html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+            <link rel="stylesheet" type="text/css" href="./style.css">
+        </head>
+        <body>
+        <h1 class="profile">${response.name}</h1>
+        <div class="profile">${response.location}</div>
+        <div class="profile">${response.bio}</div>
+        <div class="profile">${response.linkedin}</div>
+        <div class="profile">${response.github}</div>
+
+        <script type="text/javascript" src="script.js"></script>
+        </body>
+        </html>  
+    `;
+        fs.writeFile('index.html', html, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+    
+            console.log('Success!');
+        });
+
+      //const element = document.querySelectorAll("#user");
+    //  element.innerHTML = userInfo;
+    }
+);
+
