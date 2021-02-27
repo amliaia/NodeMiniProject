f1bba5d60039f49e62b94bcf536a66b25c67e10d
Dora O <dorozco2390@utexas.edu>
2021-02-27 13:00:57 -0600

    created index.js file with prompts

---
+++ index.js
@@ -0,0 +1,36 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is your password?',
+            name: 'location',
+        },
+        {
+            type: 'input',
+            message: 'Give me a brief bio:',
+            name: 'bio',
+        },
+        {
+            type: 'input',
+            message: 'What is your LinkedIn URL?',
+            name: 'linkedin',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub URL?',
+            name: 'github',
+        },
+    ])
+    .then((response) =>
+        response.confirm === response.password
+            ? console.log('Success!')
+            : console.log('You forgot your password already?!')
+    );
